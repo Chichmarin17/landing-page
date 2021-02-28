c4ff75ba0fb745f75d562102b0425bba4a42ab5a
Chichmarin17 <pits1917burg@gmail.com>
2021-02-28 19:02:57 +0300

    feat: add final version

--- app/js/start.js
+++ app/js/start.js
@@ -1,1 +1,8 @@
 // Here we write your JS code
+const burger = document.querySelector('.menu-burger');
+const menu = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger-active');
+  menu.classList.toggle('menu-active')
+})
